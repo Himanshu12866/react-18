d4fc7c6a5a906255e73d6e6b99b6f16aa355b56d
Himanshu12866 <manshu010m@gmail.com>
2024-08-07 18:41:00 +0530

    "Updated index.html, main1.jsx, main1.css, and main1.scss files with new HTML structure, CSS styles, and video element addition."

--- src/components/main1.jsx
+++ src/components/main1.jsx
@@ -1,13 +1,23 @@
 import "../styles/main1.css"
 
-export default function MainBox(){
-    return  (
+export default function MainBox() {
+    return (
         <div className="main-box">
             <div className="row">
                 <div className="col-lg-6 col-sm-12">
-<h1>Watch everywhere</h1>
+                    <div>
+                        <h1>Enjoy on your TV</h1>
+                        <h3>Watch on smart TVs, PlayStation, Xbox,</h3>
+                        <h3>Chromecast, Apple TV, Blu-ray players and more.</h3>
+                    </div>
                 </div>
-                <div className="col-lg-6 col-sm-12"></div>
+                <div className="col-lg-6 col-sm-12">
+                    <div className="imgbox">
+                        <video  autoPlay muted loop>
+                            <source src="images/vdo.m4v" ></source>
+                        </video>
+                    </div>
+                </div>
             </div>
         </div>
     )
