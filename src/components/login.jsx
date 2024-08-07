1d1294013b4f5a339975c6ce3e081eace775035f
Himanshu12866 <manshu010m@gmail.com>
2024-08-07 16:47:23 +0530

    "Updated Login component with new HTML structure and styles, and adjusted margin-top in homepage styles"

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,7 +1,12 @@
+import "../styles/login.css"
 export default function Login(){
     return (
         <div className="login-box">
-            <h4>Ready to watch? Enter your email to create or restart your membership.</h4>
+            <h5>Ready to watch? Enter your email to create or restart your membership.</h5>
+            <div className="input-box">
+                <input type="email" placeholder="Email Address" />
+                <button>Get Started <span className="bi bi-chevron-right"></span></button>
+            </div>
         </div>
     )
 }
