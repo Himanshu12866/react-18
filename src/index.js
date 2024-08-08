5170e962555c68de9919e71ad73e7eded63dd567
Himanshu12866 <manshu010m@gmail.com>
2024-08-08 16:22:54 +0530

    "Added font imports and updated font families in various CSS files"

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import bootstrap from "bootstrap";
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
