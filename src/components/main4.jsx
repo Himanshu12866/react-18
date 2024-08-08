a9a7117db769236e4ad340290af056d9f5c5edb4
Himanshu12866 <manshu010m@gmail.com>
2024-08-08 17:57:40 +0530

    "Updated MainBox4 component and main4.css to include image, text box, and styling changes"

--- src/components/main4.jsx
+++ src/components/main4.jsx
@@ -1,8 +1,21 @@
 import "../styles/main4.css"
-export default function MainBox4(){
+export default function MainBox4() {
     return (
         <div className="main-box-4">
-<h1>Jai Shri Ram</h1>
+            <div className="row">
+                <div className="col-lg-6 col-sm-12">
+                    <img alt="childs" src="images/child.png"></img>
+
+                </div>
+                <div className="col-lg-6 col-sm-12">
+                    <div className="TextBox">
+                        <h1>Create profiles for kids</h1>
+                        <p>Send children on adventures with their favourite characters in a space made just for them—free with your membership.
+
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
