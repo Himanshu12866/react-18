74883023791927e7c8d3561b5684e5c16f70bcff
Himanshu12866 <manshu010m@gmail.com>
2024-08-08 17:31:48 +0530

    "Updated MainBox3 component with new HTML structure and styles, added video and image elements, and modified CSS files to match new layout."

--- src/components/main3.jsx
+++ src/components/main3.jsx
@@ -3,8 +3,23 @@ export default function MainBox3(){
     return (
         <div className="main-box-3">
             <div className="row">
-                <div className="col-lg-6 col-sm-12"></div>
-                <div className="col-lg-6 col-sm-12"></div>
+                <div className="col-lg-6 col-sm-12">
+                    <h1>Watch everywhere</h1>
+                    <h4>
+                    Stream unlimited movies and TV shows on your
+                    </h4>
+                    <h4>
+                    phone, tablet, laptop, and TV.
+                    </h4>
+                </div>
+                <div className="col-lg-6 col-sm-12">
+                    <div className="ImgBox">
+                    <video autoPlay muted loop>
+                            <source src="images/vdo2.m4v" ></source>
+                        </video>
+                        <div className="overlay-image"></div>
+                    </div>
+                </div>
             </div>
         </div>
     )
