2937542415cf30c1e1acb6fb6087bd2f075295d9
Himanshu12866 <manshu010m@gmail.com>
2024-08-08 16:52:20 +0530

    "Updated MainBox2 component with new image layout and styling, and modified font family in main1.css"

--- src/components/main2.jsx
+++ src/components/main2.jsx
@@ -5,22 +5,34 @@ export default function MainBox2() {
         <div className="main-box2">
             <div className="row">
                 <div className="col-lg-6 col-sm-12">
-                <div className="img-box">
-                    <div className="ImgBox">
-                        <div></div>
+                    <div className="img-box">
+                        <div className="ImgBox">
+                            <div className="row">
+                                <div className="col-4">
+                                    <img alt="things" src="images/things.png" />
+                                </div>
+                                <div className="col-6">
+                                    <p>Stranger Things</p>
+                                    <span>Downloading...</span>
+                                </div>
+                                <div className="col-2">
+                                    <img alt="gif" src="images/gif.gif"/>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
-                </div>
 
                 </div>
                 <div className="col-lg-6 col-sm-12">
-                  <div>
-                  <h1>Download your shows</h1>
-                    <h1>
-                        to watch offline
-                    </h1>
-                    <h3>Save your favourites easily and always have</h3>
-                    <h3>something to watch.</h3>
-                  </div>
+                    <div>
+                        <h1>Download your shows</h1>
+                        <h1>
+                            to watch offline
+                        </h1>
+                        <h3>Save your favourites easily and always have</h3>
+                        <h3>something to watch.</h3>
+                    </div>
                 </div>
             </div>
         </div>
