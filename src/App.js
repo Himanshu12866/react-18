935c4c2d12ffe014616da2e5258a320d6f715960
Himanshu12866 <manshu010m@gmail.com>
2024-08-08 17:11:06 +0530

    "Added MainBox3 component, updated main2.css and main2.scss to increase height, and created main3.css and main3.scss with styles for MainBox3"

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './components/home';
 import MainBox from './components/main1';
 import MainBox2 from './components/main2';
+import MainBox3 from './components/main3';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <HomePage />
     <MainBox />
     <MainBox2 />
+   <MainBox3 />
    </div>
    
   );
--- src/components/main3.jsx
+++ src/components/main3.jsx
@@ -1,5 +1,11 @@
+import "../styles/main3.css"
 export default function MainBox3(){
     return (
-        <div className="main-box-3"></div>
+        <div className="main-box-3">
+            <div className="row">
+                <div className="col-lg-6 col-sm-12"></div>
+                <div className="col-lg-6 col-sm-12"></div>
+            </div>
+        </div>
     )
 }
