55b36042eca877c9ee07cfd38a4039ba5ccda1e4
Himanshu12866 <manshu010m@gmail.com>
2024-08-08 19:03:56 +0530

    "Added MainBox5 component and imported it in App.js, updated MainBox5 component with accordion functionality, and updated index.js to import bootstrap bundle"

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import MainBox from './components/main1';
 import MainBox2 from './components/main2';
 import MainBox3 from './components/main3';
 import MainBox4 from './components/main4';
+import MainBox5 from './components/main5';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
     <MainBox2 />
    <MainBox3 />
    <MainBox4 />
+   <MainBox5 />
    </div>
    
   );
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import bootstrap from "bootstrap";
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+// import bootstrap from "bootstrap";
 import reportWebVitals from './reportWebVitals';
 
 
