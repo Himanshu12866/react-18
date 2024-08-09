b49af58f7580ec2150be8b539740ebac1bb095a0
Himanshu12866 <manshu010m@gmail.com>
2024-08-09 15:37:21 +0530

    "Updated package-lock.json and package.json to include scss dependency, modified header.jsx and main5.jsx components, and updated styles in header.css, header.scss, main5.css, and main5.scss files."

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,18 @@
+// import { useState } from "react";
 import "../styles/header.css";
 
 export default function Header() {
+
+//     const [style, setStyle] = useState("none")
+
+//     function handleDisplay(){
+//     if(style === "none"){
+//         setStyle("block");
+//     }else{
+//         setStyle("none");
+//     }
+//     }
+
     return (
         <div className="headerBox">
             <div className="img-box">
@@ -9,9 +21,19 @@ export default function Header() {
             <div className="dropdownBox">
                 <div className="dropbox">
                     <select className="form-select">
-                        <option> Hindi</option>
                         <option>English</option>
+                        <option> हिन्दी</option>
                     </select>
+{/* 
+                    <ul className="text-light list-unstyled">
+                        <li onClick={handleDisplay}>
+                            English
+                        <ul className={`d-${style}`}>
+                            <li>English</li>
+                            <li>हिन्दी</li>
+                        </ul>
+                        </li>
+                    </ul> */}
                <button type="button" className="btn "> Sign In</button>
                 </div>
             </div>
--- src/components/main5.jsx
+++ src/components/main5.jsx
@@ -12,7 +12,7 @@ export default function MainBox5() {
                             </button>
                         </h2>
                         <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body"> <p style={{fontSize:"20px"}}>Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.</p>
+                            <div class="accordion-body" style={{backgroundColor:"rgb(45,45,45)"}}> <p style={{fontSize:"20px"}}>Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.</p>
                                 <p style={{fontSize:"20px"}}>You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!</p>
                             </div>
                         </div>
@@ -24,7 +24,7 @@ export default function MainBox5() {
                             </button>
                         </h2>
                         <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body"><p style={{fontSize:"20px"}}>Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹149 to ₹649 a month. No extra costs, no contracts.</p></div>
+                            <div class="accordion-body" style={{backgroundColor:"rgb(45,45,45)"}}><p style={{fontSize:"20px"}}>Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹149 to ₹649 a month. No extra costs, no contracts.</p></div>
                         </div>
                     </div>
                     <div class="accordion-item">
@@ -34,7 +34,7 @@ export default function MainBox5() {
                             </button>
                         </h2>
                         <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body"><p style={{fontSize:"20px"}}>
+                            <div class="accordion-body" style={{backgroundColor:"rgb(45,45,45)"}}><p style={{fontSize:"20px"}}>
                                 Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.
                             </p>
                                 <p style={{fontSize:"20px"}}>You can also download your favourite shows with the iOS or Android app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.</p></div>
@@ -47,7 +47,7 @@ export default function MainBox5() {
                             </button>
                         </h2>
                         <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-heading" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body"><p style={{fontSize:"20px"}}>
+                            <div class="accordion-body" style={{backgroundColor:"rgb(45,45,45)"}}><p style={{fontSize:"20px"}}>
                                 Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.
                             </p></div>
                         </div>
@@ -59,7 +59,7 @@ export default function MainBox5() {
                             </button>
                         </h2>
                         <div id="flush-collapseFive" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body"><p style={{fontSize:"20px"}}>
+                            <div class="accordion-body" style={{backgroundColor:"rgb(45,45,45)"}}><p style={{fontSize:"20px"}}>
                                 Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.
                             </p></div>
                         </div>
@@ -71,7 +71,7 @@ export default function MainBox5() {
                             </button>
                         </h2>
                         <div id="flush-collapseSix" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body"><p style={{fontSize:"20px"}}>
+                            <div class="accordion-body" style={{backgroundColor:"rgb(45,45,45)"}}><p style={{fontSize:"20px"}}>
                                 The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.
                             </p>
                                 <p style={{fontSize:"20px"}}>
