8b9f7be35346d1835637cb963fa99f82025085df
Himanshu12866 <manshu010m@gmail.com>
2024-08-09 17:52:26 +0530

    "Deleted Node.js CI workflow file and updated MainBox6 component JSX"

--- src/components/mian6.jsx
+++ src/components/mian6.jsx
@@ -1,6 +1,42 @@
 import "../styles/main6.css"
 export default function MainBox6(){
     return (
-        <div className="main-box-6"></div>
+        <div className="main-box-6">
+            <div className="row">
+                <div className="col-lg-6 col-sm-12">
+                    <div className="row">
+                        <div className="col-6">
+                            <p>
+                                <span>
+                                    <a h></a>
+                                </span>
+                            </p>
+                            <p>
+                                <span>
+
+                                </span>
+                            </p>
+                            <p>
+                                <span>
+
+                                </span>
+                            </p>
+                            <p>
+                                <span>
+
+                                </span>
+                            </p>
+                        </div>
+                        <div className="col-6"></div>
+                    </div>
+                </div>
+                <div className="col-lg-6 col-sm-12">
+                <div className="row">
+                        <div className="col-6"></div>
+                        <div className="col-6"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
