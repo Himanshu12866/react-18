607c6edb0da7e94a4f2ebab35878edf81de98473
Himanshu12866 <manshu010m@gmail.com>
2024-08-09 18:38:18 +0530

    "Updated MainBox6 component and styles: modified HTML structure, added Hindi language option, and adjusted CSS for layout and responsiveness."

--- src/components/mian6.jsx
+++ src/components/mian6.jsx
@@ -1,72 +1,77 @@
 import "../styles/main6.css"
-export default function MainBox6(){
+export default function MainBox6() {
     return (
         <div className="main-box-6">
             <div className="row">
-            <p>Questions? Call <a href="tel:+4733378901">1800 200 4000</a></p>
+                <p>Questions? Call <a href="tel:+4733378901">1800 200 4000</a></p>
                 <div className="col-lg-6 col-sm-12">
                     <div className="row">
                         <div className="col-6">
                             <p>
-                               <a href="https://help.netflix.com/support/412">FAQ</a>
+                                <a href="https://help.netflix.com/support/412">FAQ</a>
                             </p>
                             <p>
-                              <a href="https://help.netflix.com/support/412">Invester Relation</a>
+                                <a href="https://help.netflix.com/support/412">Invester Relation</a>
                             </p>
                             <p>
-                              <a href="https://help.netflix.com/support/412">Privacy</a>
+                                <a href="https://help.netflix.com/support/412">Privacy</a>
                             </p>
                             <p>
-                              <a href="https://help.netflix.com/support/412">Speed Test</a>
+                                <a href="https://help.netflix.com/support/412">Speed Test</a>
                             </p>
-                            <select>
-                                <option>English</option>
-                                <option>Hindi</option>
-                            </select>
+                            <p>
+                                <select>
+                                    <option>English</option>
+                                    <option>हिन्दी</option>
+                                </select>                         
+                           </p>
+                           <p>Netflix India</p>
+
                         </div>
                         <div className="col-6">
-                        <p>
-                               <a href="https://help.netflix.com/support/412">Help Center</a>
+                            <p>
+                                <a href="https://help.netflix.com/support/412">Help Center</a>
                             </p>
                             <p>
-                              <a href="https://help.netflix.com/support/412">Jobs</a>
+                                <a href="https://help.netflix.com/support/412">Jobs</a>
                             </p>
                             <p>
-                              <a href="https://help.netflix.com/support/412">Cookies Preference</a>
+                                <a href="https://help.netflix.com/support/412">Cookies Preference</a>
                             </p>
                             <p>
-                              <a href="https://help.netflix.com/support/412">Legal Notices</a>
+                                <a href="https://help.netflix.com/support/412">Legal Notices</a>
                             </p>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-6 col-sm-12">
-                <div className="row">
+                    <div className="row">
                         <div className="col-6">
-                        <p>
-                               <a href="https://help.netflix.com/support/412">Account</a>
+                            <p>
+                                <a href="https://help.netflix.com/support/412">Account</a>
                             </p>
                             <p>
-                              <a href="https://help.netflix.com/support/412">Ways to Watch</a>
+                                <a href="https://help.netflix.com/support/412">Ways to Watch</a>
                             </p>
                             <p>
-                              <a href="https://help.netflix.com/support/412">Corporate Information</a>
+                                <a href="https://help.netflix.com/support/412">Corporate Information</a>
                             </p>
                             <p>
-                              <a href="https://help.netflix.com/support/412">Only one Netflix</a>
+                                <a href="https://help.netflix.com/support/412">Only one Netflix</a>
                             </p>
                         </div>
                         <div className="col-6">
-                        <p>
-                               <a href="https://help.netflix.com/support/412">Media Center</a>
+                            <p>
+                                <a href="https://help.netflix.com/support/412">Media Center</a>
                             </p>
                             <p>
-                              <a href="https://help.netflix.com/support/412">Terms of Use</a>
+                                <a href="https://help.netflix.com/support/412">Terms of Use</a>
                             </p>
                             <p>
-                              <a href="https://help.netflix.com/support/412">Contact Us</a>
+                                <a href="https://help.netflix.com/support/412">Contact Us</a>
                             </p>
-                           
+                            
+
                         </div>
                     </div>
                 </div>
