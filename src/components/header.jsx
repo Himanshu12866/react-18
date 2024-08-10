9b10761ba7419ce4e3d9b4badeb2dd480ce65faa
Himanshu12866 <manshu010m@gmail.com>
2024-08-10 19:07:41 +0530

    "Updated header and homepage styles, refactored JSX and CSS, and removed unnecessary imports."

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -15,16 +15,18 @@ export default function Header() {
 
     return (
         <div className="headerBox">
-            <div className="img-box">
-              
-            </div>
-            <div className="dropdownBox">
-                <div className="dropbox">
-                    <select className="form-select">
+            <div className="rowi">
+                <div className="divi">
+                    <img alt="anem" className="None" src="images/netflixlogo.png"></img>
+                </div>
+                
+                <div className="divi">
+                <span className="bi bi-translate text-light"></span>
+                    <select>
                         <option>English</option>
                         <option> हिन्दी</option>
                     </select>
-               <button type="button" className="btn "> Sign In</button>
+                    <button className="btnn">Signin</button>
                 </div>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
-// import bootstrap from "bootstrap";
 import reportWebVitals from './reportWebVitals';
 
 
@@ -16,7 +15,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
