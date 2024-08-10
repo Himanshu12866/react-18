4baa94abbc404f80c5f2648e6c519b2c2a331861
Himanshu12866 <manshu010m@gmail.com>
2024-08-10 15:39:36 +0530

    "Updated MainBox3 component and styles: added image to overlay, modified video and image styles, and added media query for mobile layout."

--- src/components/main3.jsx
+++ src/components/main3.jsx
@@ -1,23 +1,25 @@
 import "../styles/main3.css"
-export default function MainBox3(){
+export default function MainBox3() {
     return (
         <div className="main-box-3">
             <div className="row">
                 <div className="col-lg-6 col-sm-12">
                     <h1>Watch everywhere</h1>
                     <h4>
-                    Stream unlimited movies and TV shows on your
+                        Stream unlimited movies and TV shows on your
                     </h4>
                     <h4>
-                    phone, tablet, laptop, and TV.
+                        phone, tablet, laptop, and TV.
                     </h4>
                 </div>
                 <div className="col-lg-6 col-sm-12">
                     <div className="ImgBox">
-                    <video autoPlay muted loop>
+                        <video autoPlay muted loop>
                             <source src="images/vdo2.m4v" ></source>
                         </video>
-                        <div className="overlay-image"></div>
+                        <div className="overlay-image">
+                            <img alt="name" src="images/tv2.png"></img>
+                        </div>
                     </div>
                 </div>
             </div>
