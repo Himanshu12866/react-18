2a32dae4afcf9e007ba8a2439f64363eb8a895aa
Himanshu12866 <manshu010m@gmail.com>
2024-08-15 19:25:32 +0530

    "Added ClickMe function and event handler to login button, modified input field to be required"

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,11 +1,21 @@
 import "../styles/login.css"
-export default function Login(){
+export default function Login() {
+    function ClickMe(e) {
+      alert(`
+        
+        BUtton Id : ${e.target.id} 
+        Button className : ${e.target.className} 
+        X Position : ${e.clientX} 
+        Y Position : ${e.clientY} 
+
+        `)
+    }
     return (
         <div className="login-box">
             <h5>Ready to watch? Enter your email to create or restart your membership.</h5>
             <div className="input-box">
-                <input type="email" placeholder="Email Address" />
-                <button>Get Started <span className="bi bi-chevron-right"></span></button>
+                <input required type="email" placeholder="Email Address" />
+                <button id="BtnId" className="BtnClassName" onClick={ClickMe}>Get Started <span className="bi bi-chevron-right"></span></button>
             </div>
         </div>
     )
